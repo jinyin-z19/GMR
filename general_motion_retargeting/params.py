6258b676ba7611de8160bae3944f215626f60b53
jinyin-z19 <19807733522@163.com>
2026-05-01 21:54:57 +0800

    agent add loong

--- general_motion_retargeting/params.py
+++ general_motion_retargeting/params.py
@@ -23,6 +23,7 @@ ROBOT_XML_DICT = {
     "tienkung": ASSET_ROOT / "tienkung" / "mjcf" / "tienkung.xml",
     "pal_talos": ASSET_ROOT / "pal_talos" / "talos.xml",
     "fourier_gr3": ASSET_ROOT / "fourier_gr3v2_1_1" / "mjcf" / "gr3v2_1_1_dummy_hand.xml",
+    "azureloong_v9": ASSET_ROOT / "azureloong_v9" / "azureloong_v9.xml",
 }
 
 IK_CONFIG_DICT = {
@@ -45,6 +46,7 @@ IK_CONFIG_DICT = {
         "pnd_adam_lite": IK_CONFIG_ROOT / "smplx_to_adam.json",
         "tienkung": IK_CONFIG_ROOT / "smplx_to_tienkung.json",
         "fourier_gr3": IK_CONFIG_ROOT / "smplx_to_gr3.json",
+        "azureloong_v9": IK_CONFIG_ROOT / "bvh_nokov_to_azureloong_v9.json",
     },
     "bvh_lafan1":{
         "unitree_g1": IK_CONFIG_ROOT / "bvh_lafan1_to_g1.json",
@@ -57,6 +59,7 @@ IK_CONFIG_DICT = {
     },
     "bvh_nokov":{
         "unitree_g1": IK_CONFIG_ROOT / "bvh_nokov_to_g1.json",
+        "azureloong_v9": IK_CONFIG_ROOT / "bvh_nokov_to_azureloong_v9.json",
     },
     "bvh_xsens":{
         "unitree_g1": IK_CONFIG_ROOT / "bvh_xsens_to_g1.json",
@@ -98,6 +101,7 @@ ROBOT_BASE_DICT = {
     "tienkung": "Base_link",
     "pal_talos": "base_link",
     "fourier_gr3": "base_link",
+    "azureloong_v9": "base_link",
 }
 
 VIEWER_CAM_DISTANCE_DICT = {
@@ -119,4 +123,5 @@ VIEWER_CAM_DISTANCE_DICT = {
     "tienkung": 3.0,
     "pal_talos": 3.0,
     "fourier_gr3": 2.0,
+    "azureloong_v9": 2.0,
 }
